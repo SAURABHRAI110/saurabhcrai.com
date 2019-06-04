9d8dd69a1051be8790e0dbd82180edec644fa7a0
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-06-05 01:12:01 +0530

    +initial home layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,50 @@
 <template>
   <div>
-    <h1>My Love</h1>
-    <div data-aos="slide-up">
-      <img src="~/assets/img/home-people-4.jpg" />
+    <div class="vl1 vl"></div>
+    <div class="vl2 vl"></div>
+    <div class="vl3 vl"></div>
+
+    <div class="nametag-container contain">
+      <div class="h-name">Saurabh Rai</div>
+      <div class="h-j-title">UI/UX Designer | Photographer | Artist</div>
     </div>
+    <!-- <div class="h-nametag">
+      <div class="nametag1">
+        <p
+          class="nametag-text black61"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          Saurabh Rai
+        </p>
+      </div>
+      <div class="nametag2">
+        <p
+          class="black61 nametag-text"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          UI/UX Designer | Photographer | Artist
+        </p>
+      </div> 
+    </div>-->
     <div
-      v-rellax="{
-        speed: -2
-      }"
+      class="contain h-hero-title"
+      data-aos="fade-up"
+      data-aos-duration="2500"
     >
-      <img src="~/assets/img/home-people-5.jpg" />
+      <h1>
+        <span>Experience</span>
+        <br />
+        <span class="outlined">Designer</span> At <br />Feeding Trends
+      </h1>
+    </div>
+    <div class="contain">
+      <p data-aos="fade-up" data-aos-duration="2500" data-aos-delay="1000">
+        This website is my personal space where I'm trying to express my
+        creativity through
+        <span style="color:#fc3a52;">design, code and writing.</span>
+      </p>
     </div>
   </div>
 </template>
@@ -18,4 +53,9 @@
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.h-hero-title {
+  white-space: nowrap;
+  margin-top: 12%;
+}
+</style>
