c92a15b4b7859616a1a542c7704dcc1b25d1e15c
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-06-21 22:24:39 +0530

    +fe
    Components

---
+++ components/projects/pcase.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contain">
+    <div class="reveal-holder" data-aos="fade-up" data-aos-duration="1000">
+      <div class="reveal-block red right" data-aos="reveal-right"></div>
+      <h2 class="p-para black50">{{heading}}</h2>
+    </div>
+    <p
+      class="black90"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      data-aos-delay="300"
+      v-html="para"
+    ></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: String,
+    para: String
+  }
+}
+</script>
+
+<style scoped>
+p {
+  padding-bottom: 50px;
+}
+</style>
