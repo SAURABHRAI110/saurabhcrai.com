745e7112ece7018aaa84b880530217aa7832c61f
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-09-09 22:27:46 +0530

    Project:Hindigiri: Initial pics and layout

--- pages/projects/hindigiri.vue
+++ pages/projects/hindigiri.vue
@@ -1,10 +1,237 @@
-<template></template>
+<template>
+  <!-- r3 work -->
+  <div>
+    <div class="ppnp-container">
+      <nuxt-link to="/projects/awesome-inc">
+        <next />
+      </nuxt-link>
+      <nuxt-link to="/projects/mr-irritating">
+        <previous />
+      </nuxt-link>
+    </div>
+    <projecthero
+      pDate="October 2017 "
+      pWebsite="Hindigiri"
+      pCategory="Literature Society"
+      pHeading="Hindigiri: empowering poeple with hindi"
+      pHeadingcolor="color:#25A9E0;"
+      pSubheading="to thousands of people  through Campaign Design."
+    />
+    <div class="p-dz-hero_image">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div
+          class="reveal-block right theme_hindigiri"
+          data-aos="reveal-right"
+          data-aos-once="true"
+        ></div>
+        <img src="~assets/projects/hindigiri/hindigiri-hero.jpg" alt="r3 facebook mockup" />
+      </div>
+    </div>
+    <Preface
+      timeframe=" October 2017
+            <br> 1 Week"
+      tools="   Adobe Illustrator,
+            <br>Love and Time,"
+      role="  Graphic Designer
+            <br>Researcher"
+      team=" Shashikant Singh
+            <br>Me and Hindigiri Family"
+    />
+    <!-- Preface -->
+    <pcase
+      heading="Preface"
+      para="R3 is a platform for the writers/artists to express their thoughts.
+      Share them with the world - expressions that will connect with everyone,
+      rhythms that will resonate with every heart.
+      In three beautiful linguistics - English, Hindi & Urdu, with Love R3."
+      theme="theme_hindigiri"
+    />
+    <div class="p-quote contain red">
+      <blockquote>
+        Human mind percieves about 60k to 80k thoughts per day. All we wanted was one or two thoughts per day.
+        expressions that will connect with everyone, rhythms that will resonate with every heart.
+      </blockquote>
+    </div>
+
+    <p class="contain just-para black90">
+      The purpose of doing all this was to have a presence on Instagram and facebook. A thought provoking page or account presented in a beautiful
+      way with a consistent design and voice tone.
+    </p>
+    <!-- Problem -->
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source media="(max-width: 600px)" srcset="~assets/projects/hindigiri/hindigiri-2.jpg" />
+          <img src="~assets/projects/hindigiri/hindigiri-2.jpg" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source media="(max-width: 600px)" srcset="~assets/projects/hindigiri/hindigiri-3.jpg" />
+          <img src="~assets/projects/hindigiri/hindigiri-3.jpg" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source media="(max-width: 600px)" srcset="~assets/projects/hindigiri/hindigiri-4.jpg" />
+          <img src="~assets/projects/hindigiri/hindigiri-4.jpg" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source media="(max-width: 600px)" srcset="~assets/projects/hindigiri/hindigiri-5.jpg" />
+          <img src="~assets/projects/hindigiri/hindigiri-5.jpg" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source media="(max-width: 600px)" srcset="~assets/projects/hindigiri/hindigiri-6.jpg" />
+          <img src="~assets/projects/hindigiri/hindigiri-6.jpg" />
+        </picture>
+      </div>
+    </div>
+
+    <!-- key Focus Area -->
+
+    <div class="keyfocus">
+      <div class="content">
+        <div class="contain">
+          <h2 class="p-para black50">Stuff I did</h2>
+
+          <div class="pm black70">
+            <div class="kf-layout">
+              <span>✔ Research</span>
+              <span>✔ Ideation</span>
+              <span>✔ Taking Design Meetings</span>
+              <span>✔ Testing Different post size</span>
+              <span>✔ Taking feedback</span>
+              <span>✔ Lots of IceTea</span>
+              <span>✔ Created Design System</span>
+              <span>✔ Logo Design</span>
+              <span>✔ Testing with users</span>
+              <span>✔ Created Initial Post</span>
+              <span>✔ Branding</span>
+
+              <span>✔ Collaborated with content writers</span>
+              <span>✔ Delete Useles Stuffs</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- conclusion -->
+
+    <div class="Takeaway">
+      <div class="contain">
+        <h2 class="p-para black50">Learnings</h2>
+        <div class="pm black70">
+          <p>
+            Readability over everything.
+            A lot of things goes in and out even for a simple design. Just creating a logo. Sometimes it just happen in min or so sometimes it requires
+            lot of work through discovery. This one project was one of kind where readability matters most.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="p-quote contain red">
+      <blockquote>
+        The number one factor in engagement is relevance,
+        because relevance drives out resistance.
+      </blockquote>
+    </div>
+
+    <!-- End Line  -->
+
+    <div class="section-divider">
+      <hr class="section-divider" />
+    </div>
+
+    <!-- appreciate button -->
+
+    <Appreciate
+      heading="Share Your thoughts "
+      subHeading="Damn you really like to scroll. Click Me."
+    />
+
+    <!-- Project Footer Navigatrion -->
+
+    <div class="project-footer">
+      <div class="projects-footer-container">
+        <allprojectsnav />
+        <div class="p-f-p-container">
+          <nuxt-link to="/projects/mr-irritating">
+            <pfooterp
+              projectName="Mr. Irritating"
+              projectCategories="Print, Social Media"
+              thumbnail="/showcase3.webp"
+            />
+          </nuxt-link>
+        </div>
+
+        <div class="p-f-n-container">
+          <nuxt-link to="/projects/awesome-inc">
+            <pfootern
+              projectName="Awesome Inc"
+              projectCategories="Identity, Print"
+              thumbnail="/showcase5b.webp"
+            />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
+import Projecthero from '~/components/projects/projecthero'
+import Preface from '~/components/projects/preface'
+import Projectfooter from '~/components/projects/projectfooter'
+import Pcase from '~/components/projects/pcase'
+import Appreciate from '~/components/projects/appreciate'
+import Next from '~/components/projects/next'
+import Previous from '~/components/projects/previous'
+import Pfootern from '~/components/projects/pfootern'
+import Pfooterp from '~/components/projects/pfooterp'
+import Allprojectsnav from '~/components/projects/allprojectsnav'
 export default {
-  layout: 'projects'
+  layout: 'projects',
+  components: {
+    Projectfooter,
+    Preface,
+    Appreciate,
+    Pcase,
+    Projecthero,
+    Next,
+    Previous,
+    Pfooterp,
+    Pfootern,
+    Allprojectsnav
+  }
 }
 </script>
 
 <style>
+.project-footer {
+  bottom: 0%;
+}
 </style>
