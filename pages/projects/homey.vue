807c5590092875957613b1ba116553e9a8ebf5a1
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-11-28 17:11:37 +0530

    +Project: Homey

---
+++ pages/projects/homey.vue
@@ -0,0 +1,534 @@
+<template>
+  <!-- Feeding Trends -->
+  <div>
+    <div class="ppnp-container">
+      <nuxt-link to="my-portfolio-website">
+        <previous />
+      </nuxt-link>
+
+      <nuxt-link to="elevito">
+        <next />
+      </nuxt-link>
+
+      <div class="ppnp-number-container">
+        <p class="black ppnp-p-number">
+          <span style="color:#0088FF;">01</span>
+          <span>/</span>
+          <span>20</span>
+        </p>
+      </div>
+    </div>
+
+    <projecthero
+      pDate="November 2019 "
+      pWebsite="Homey"
+      pCategory="Web, Branding"
+      pHeading="Homey: Home to thousands of rentals"
+      pHeadingcolor="color:#0088FF;"
+      pSubheading="Web Design for Homey."
+    />
+
+    <!-- hero image -->
+    <div class="p-dz-hero_image">
+      <div class="reveal-holder" data-aos="fade-up">
+        <div class="reveal-block right theme_cricbuzz" data-aos="reveal-right"></div>
+
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-hero-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-hero.jpg"
+            alt="woman t-shirt, woman t-shirt mockup"
+            align
+          />
+        </picture>
+      </div>
+    </div>
+
+    <Preface
+      timeframe=" November 2019
+            <br>2 Days"
+      tools="Adobe XD
+            <br>Adobe Photoshop"
+      role="  UX/UI Design
+            <br>Content Strategist"
+      team=" Lone wolf
+            <br>Me only"
+    />
+    <!-- Preface -->
+    <pcase
+      heading="Preface"
+      para=" Cricbuzz app is used by millions of user. To redesign something that that
+      is already have a existing user base is itself is a challenge. I started with
+      installing the app from play store and then carefully following each navigation. 
+      I personally used this app previously some years ago not now.
+      So much has already been changed and improved from then.
+      "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="contain">
+      <div class="p-image-100-duo">
+        <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+          <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+          <picture>
+            <img src="~assets/projects/cricbuzz/cricbuzz-logo.jpg" alt="Crick buzz app redesign" />
+          </picture>
+        </div>
+      </div>
+    </div>
+
+    <pcase
+      heading="Challenge"
+      para=" 1. Strict Deadline <br>
+      2. Did it for an Interview <br>
+      3. Only Two days to showcase UI/UX Process <br>
+      4. No time for research
+  
+        
+     "
+      theme="Research"
+    />
+
+    <div class="p-quote contain red">
+      <blockquote>
+        Cricbuzz app is used by millions of user. To redesign something that that
+        is already have a existing user base is itself is a challenge.
+      </blockquote>
+    </div>
+
+    <!-- Desighn -->
+
+    <pcase
+      heading="Research"
+      para="Since time was less and so couldn't follow design process and totally skipped
+      user research. But I did talk to some friends and asked their existing experience of
+      using Cricbuzz app and their pain-points.
+
+      <br>
+      <br>
+      
+      So instead I carefully observed each screen, realized what's missing and what can be
+      done to make user experience more intuitive.
+
+      <br>
+      <br>
+
+      One thing that really needed to be redesign was app navigation. If I have to go
+      from one screen to another, it was really difficult to manage them with one handed
+      with thumb only. I love Samsung one UI and based on that I decided to restructure
+      every screen of cricbuzz app also.
+
+      
+        
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <!-- Desighn -->
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-existing-screens-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-existing-screens.jpg"
+            alt="cricbuzz app screenshots"
+          />
+          <p class="ps">Some cricbuzz app screenshots</p>
+        </picture>
+      </div>
+    </div>
+
+    <!-- Problems and initial Findings -->
+    <pcase
+      heading="Problems and Initial Findings"
+      para=" 
+      1. No Clear structure and navigation flows <br>
+      2. Cluttered UI <br>
+      3. Very less white space <br>
+      4. Navigation problems on bigger screen <br>
+      5. Not one handed friendly <br>
+      6. Bad Content Architecture and Readability <br>
+      7. Very little transition between app screen in so user get distracted from going to one screen to another.<br>
+      "
+      theme="theme_cricbuzz"
+    />
+
+    <!-- Desighn -->
+    <pcase
+      heading="UX Principles"
+      para=" Through out designing the app I followed these UX Principles specially
+      user touch behavior and Fitts's Law.
+
+            
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo cricbuzz-ux-image">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+
+        <img src="~assets/projects/dreamz/dreamz-laws-of-ux.jpg" alt="ux principles and laws" />
+        <p class="ps black">Applied UX Principles</p>
+      </div>
+    </div>
+
+    <!-- Problems -->
+    <pcase
+      heading="Wireframing"
+      para=" To get started I did a very rapid wireframing. With just important
+      screen and feature I thought of adding. Don't mind my handwriting.
+      Its way better originally. 
+               
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-wireframing.jpg"
+            alt="cricbuzz app wireframing"
+          />
+        </picture>
+      </div>
+    </div>
+
+    <!-- User Interface -->
+    <pcase
+      heading="User Interface"
+      para="I started designing app design with splash screen. Made a new logo very quickly.
+      Well existing logo is much good though. Anyway then choose a font i.e. Montserrat.
+      There is something me and my friends say, when in doubt use Montserrat. Its a beautiful
+      font. As I have not much time so I didn't experimented with different fonts.  
+               
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-xd-all-screen.jpg"
+            alt="cricbuzz app redesign ui screen"
+          />
+          <p class="ps black70">Adobe XD- App screens</p>
+        </picture>
+      </div>
+    </div>
+
+    <pcase
+      heading="Onboarding Experience"
+      para="In the existing app there was no onboarding experience and very little
+          personalization. Personalization can be used for collecting data in terms
+          of business and better user experience. Can be used for Push notification
+          and also so that user can never miss matches from his/her favorite
+          team and also to not miss stories from his/her favorite player.
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-ui-onboarding-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-ui-onboarding.jpg"
+            alt="cricbuzz app redesign onboarding experience screen"
+          />
+          <p class="ps black70">App Onboarding experience</p>
+        </picture>
+      </div>
+    </div>
+
+    <pcase
+      heading="Home"
+      para="Home is now less cluttered with more white space and better readability.
+      Often times we just wanted to see just the latest scores and so the first thing on
+      home he will focus will be on bigger typos i.e. the score following player current
+      run and likewise 
+
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-ui-home-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-ui-home.jpg"
+            alt="cricbuzz app redesign home screen"
+          />
+          <p class="ps black70">App Home Screen Instances</p>
+        </picture>
+      </div>
+    </div>
+
+    <pcase
+      heading="Other App Screen"
+      para="Every Interactive elements is placed in the Active area of the mobile wherever possible
+      so even a person who is holding phone in one hand can navigate with thumb easily just like in Samsung new one UI. With new search feature
+      user can easily go through topics, related videos, player profile etc. 
+
+      All menu CTA is placed in in Active area so user can navigate very easily
+      even on bigger screen.
+
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-ui-other-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-ui-other.jpg"
+            alt="cricbuzz app redesign ui screen"
+          />
+          <p class="ps black70">App other screen UI</p>
+        </picture>
+      </div>
+    </div>
+
+    <!-- User Interface -->
+    <pcase
+      heading="Prototype"
+      para="Made an App rapid prototype showing interactions with elements, sleek animations using Adobe XD auto-animate feature. A lot can 
+      be improved though.In two days This is all did.
+.  
+               
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-xd-prototype-screen.jpg"
+            alt="cricbuzz app redesign prototype"
+          />
+          <p class="ps black70">Adobe XD-Prototype</p>
+        </picture>
+      </div>
+    </div>
+
+    <div class="contain">
+      <no-ssr>
+        <vimeo-player
+          class="vimeo"
+          ref="player"
+          :options="{ responsive: true }"
+          :video-id="372189216"
+          @ready="onReady"
+          :autoplay="false"
+          :player-height="320"
+          :player-width="640"
+          loop="1"
+        />
+      </no-ssr>
+      <!-- <iframe
+        src="https://player.vimeo.com/video/372189216"
+        width="auto"
+        height="600"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        allowfullscreen
+      ></iframe>-->
+    </div>
+
+    <pcase
+      heading="Conclusion"
+      para="There a lot of things and conditions that these design doesn't justify. With two sleepless
+      night this is all I can design. There may be one and more typos error and not pixel perfect and
+      please don't mind my English and English Grammar. I try hard though to not  mistake. There's a
+      lot of area where with good research and planning app can be made even more awesome for all
+      cricket lover.  Thanks SuperGaming.
+.  
+               
+     "
+      theme="theme_cricbuzz"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/cricbuzz/cricbuzz-mockup-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/cricbuzz/cricbuzz-mockup.jpg"
+            alt="cricbuzz app redesign mockup"
+          />
+        </picture>
+      </div>
+    </div>
+
+    <!-- key Focus Area -->
+
+    <div class="keyfocus">
+      <div class="content">
+        <div class="contain">
+          <h2 class="p-para black50">Stuff I did</h2>
+
+          <div class="pm black70">
+            <div class="kf-layout">
+              <span>✔ Research</span>
+              <span>✔ Ideation</span>
+              <span>✔ Downloaded Apps</span>
+              <span>✔ Taken screenshots</span>
+              <span>✔ User Interface</span>
+              <span>✔ User Experience</span>
+              <span>✔ Basic Wireframe</span>
+              <span>✔ Interactive Prototype</span>
+              <span>✔ Made a simple logo</span>
+              <span>✔ Rapid Prototyping</span>
+              <span>✔ Made Mockups</span>
+              <span>✔ Pop and un-pop things</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- conclusion -->
+
+    <div class="Takeaway">
+      <div class="contain">
+        <h2 class="p-para black50">Learnings</h2>``
+        <div class="pm black70">
+          <p>
+            The most challenging part of any project which I have observed is even starting it.
+            Once I start it the next big thing is Deciding which problem to solve. As a UX designer, we are
+            driven by curiosity and a burning desire to solve problems. The more insights we uncover,
+            the more problems we will identify — and the harder it becomes to settle on just one. During research and design phase
+            i got into loop of deciding what to give more time and which to not. If i have focused on all parts of Cricbuzz app, it would not have
+            been possible to finish it all in 2 days at all. So focusing only on Important stuff is very crucial.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="p-quote contain red">
+      <blockquote>
+        It’s inevitable that we uncover new pain-points along the way, but we can’t
+        change tack every time this occurs. The challenge lies in staying focused.
+      </blockquote>
+    </div>
+
+    <!-- End Line  -->
+
+    <div class="section-divider">
+      <hr class="section-divider" />
+    </div>
+
+    <!-- appreciate button -->
+    <Appreciate
+      heading="Share Your thoughts "
+      subHeading="Damn you really like to scroll. Click Me."
+    />
+
+    <!-- Project Footer Navigatrion -->
+
+    <div class="project-footer">
+      <div class="projects-footer-container">
+        <allprojectsnav />
+        <div class="p-f-p-container">
+          <nuxt-link to="/projects/my-portfolio-website">
+            <pfooterp
+              projectName="My Portfolio Website"
+              projectCategories="Web design and Development"
+              thumbnail="/my-website-thumbnail.jpg"
+            />
+          </nuxt-link>
+        </div>
+
+        <div class="p-f-n-container">
+          <nuxt-link to="/projects/elevito">
+            <pfootern
+              projectName="Elevito"
+              projectCategories="Web design, Branding, Print"
+              thumbnail="/elevito-thumbnail.jpg"
+            />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Projecthero from '~/components/projects/projecthero'
+import Preface from '~/components/projects/preface'
+import Projectfooter from '~/components/projects/projectfooter'
+import Pcase from '~/components/projects/pcase'
+import Appreciate from '~/components/projects/appreciate'
+import Next from '~/components/projects/next'
+import Previous from '~/components/projects/previous'
+import Pfootern from '~/components/projects/pfootern'
+import Pfooterp from '~/components/projects/pfooterp'
+import Allprojectsnav from '~/components/projects/allprojectsnav'
+
+export default {
+  layout: 'projects',
+  components: {
+    Projectfooter,
+    Preface,
+    Appreciate,
+    Pcase,
+    Projecthero,
+    Next,
+    Previous,
+    Pfooterp,
+    Pfootern,
+    Allprojectsnav
+  }
+}
+</script>
+
+<style>
+.project-footer {
+  bottom: 0%;
+}
+
+.cricbuzz-ux-image {
+  filter: hue-rotate(45deg);
+}
+.cricbuzz-ux-image img {
+  filter: saturate(1.4);
+}
+
+.vimeo {
+  /* height: 100%; */
+  width: 300px;
+  margin: 20px;
+}
+</style>
