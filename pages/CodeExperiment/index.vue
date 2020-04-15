ae5ee6cfae7753c141535cd92d6a15f92ea70a58
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2020-04-16 03:32:36 +0530

    Code-Experiment +Gradient Animation

--- pages/CodeExperiment/index.vue
+++ pages/CodeExperiment/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <div class="exp-hero"></div>
+    <div class="exp-hero" id="gradient">
+      <h1>
+        <span class="dot">Experiment</span>
+        <span class="dot">Fail</span>
+        <span class="dot">Learn</span>
+        <span>Repeat</span>
+      </h1>
+
+      <div class="center-h-v">
+        <button>Click Me!</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,9 +21,35 @@ export default {}
 
 <style>
 .exp-hero {
-  height: 90vh;
+  height: 100vh;
   width: 100%;
   position: relative;
   background-color: black;
+  white-space: nowrap;
+}
+
+.exp-hero h1 {
+  padding: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  margin: 0;
+  color: white;
+  font-size: 3rem;
+}
+.exp-hero > h1 > .dot {
+  padding: 0px 20px 0px 0px;
+}
+.exp-hero > h1 > .dot::after {
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+  top: 45%;
+  margin-left: 15px;
+  transform: translate(-50%);
 }
 </style>
