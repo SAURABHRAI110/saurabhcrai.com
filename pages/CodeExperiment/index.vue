64c55a9e5b1eced395cf83d2382352df87340893
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2020-04-18 20:53:33 +0530

    Exp grid

--- pages/CodeExperiment/index.vue
+++ pages/CodeExperiment/index.vue
@@ -8,15 +8,42 @@
         <span>Repeat</span>
       </h1>
 
-      <div class="center-h-v">
-        <button>Click Me!</button>
+      <div class="exp-bg-text outlined" v-rellax="{ speed: -3, center: 0.5 }">
+        <span>CODE</span>
+      </div>
+      <button @click="scrollToElement">scroll to me</button>
+    </div>
+
+    <!-- section 2 -->
+    <div class="exp-section-two">
+      <div class="exp-cards">
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
+        <div class="exp-card"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    scrollToElement() {
+      const el = this.$el.getElementsByClassName('exp-section-two')[0]
+
+      if (el) {
+        el.scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+    }
+  }
+}
 </script>
 
 <style>
@@ -51,5 +78,40 @@ export default {}
   top: 45%;
   margin-left: 15px;
   transform: translate(-50%);
+}
+
+.exp-bg-text {
+  font-size: 180px;
+  position: absolute;
+  top: 92px;
+  opacity: 0.4;
+  z-index: 1;
+  text-align: center;
+  width: 100%;
+}
+
+/* section 2 */
+
+.exp-section-two {
+  width: 100%;
+  position: relative;
+}
+
+.exp-cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(45vw, 1fr));
+  width: 100%;
+  grid-gap: 20px;
+}
+
+.exp-card {
+  height: 50vh;
+}
+
+.exp-card:nth-child(2n) {
+  background-color: red;
+}
+.exp-card:nth-child(2n-1) {
+  background-color: rgb(241, 92, 92);
 }
 </style>
