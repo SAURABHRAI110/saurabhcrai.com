e114615b76b7a1982201dffc177e9263defea9f4
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2022-05-07 02:11:41 +0530

    +BikeShikes Project

---
+++ pages/projects/bikeshikes-ecommerce.vue
@@ -0,0 +1,310 @@
+<template>
+  <!--awesome inc work -->
+
+  <div>
+    <div class="ppnp-container">
+      <nuxt-link to="dear-zindagi">
+        <previous />
+      </nuxt-link>
+
+      <nuxt-link to="tbt-app">
+        <next />
+      </nuxt-link>
+
+      <div class="ppnp-number-container">
+        <p class="black ppnp-p-number">
+          <span style="color:#A05EE5;">11</span>
+          <span>/</span>
+          <span>20</span>
+        </p>
+      </div>
+    </div>
+    <projecthero
+      pDate="June 2021 "
+      pWebsite="BikeShikes"
+      pCategory="eCommerce"
+      pHeading="Making shopping fun, fast, and easy for bicycle users."
+      pHeadingcolor="color:#ff0000;"
+      pSubheading=" eCommerce Website Design Case Study"
+    />
+    <div class="p-dz-hero_image">
+      <div class="reveal-holder" data-aos="fade-up">
+        <div class="reveal-block right theme_ai" data-aos="reveal-right"></div>
+
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-hero-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/awesome-inc/awesome-inc-hero.jpg"
+            alt="woman tshirt, woman tshirt mockup"
+          />
+        </picture>
+      </div>
+    </div>
+
+    <Preface
+      timeframe="June 2021- <br> July 2021"
+      tools="  Adobe Creative Suite, Marvel, Invision
+            <br>Pen, paper"
+      role="UX Researcher, UI/UX Designer
+
+            <br>Designing Bicycle eCommerce from concept to delivery"
+      team=" Only me
+            <br>It was lonely time"
+    />
+    <!-- Preface -->
+    <pcase
+      heading="Preface"
+      para="BikeShikes is a bicycle online store that offers many brands
+      bicycle from categories like MTB, Road, Hybrid, eBike etc. It also
+      offers accessories and clothing. The typical user is between 12-45
+      years old, and most users are students or early career professionals.
+      BikeShikes goal is to make shopping fun, fast, and easy for
+      all types of users."
+
+      
+      theme="theme_ai"
+    />
+
+    <!-- Challenge -->
+    <pcase
+      heading="The problem"
+      para="Available online shopping websites have cluttered designs, Lots
+      of technical terms, inefficient systems for browsing through products,
+      and confusing filtering out for products and checkout processes."
+      theme="theme_ai"
+    />
+  <!-- Goal -->
+    <pcase
+      heading="The Goal"
+      para="Design BikeShikes website to be user friendly by providing
+      clear navigation, clear context for technical terms, required filter
+      options and offering a fast checkout process."
+      theme="theme_ai"
+    />
+
+    <!-- Challenge -->
+    <pcase
+      heading="Understanding the user"
+      para="User research <br>
+Personas <br>
+Problem statements <br>
+User journey maps
+"
+      theme="theme_ai"
+    />
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-branding-mobile.jpg"
+          />
+          <img src="~assets/projects/awesome-inc/awesome-inc-branding.jpg" alt="branding layout" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-logo-mobile.jpg"
+          />
+          <img src="~assets/projects/awesome-inc/awesome-inc-logo.jpg" alt="awesome logo" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+
+        <img src="~assets/projects/awesome-inc/awesome-inc-logo-light.jpg" alt="awesome logo" />
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-vc-mobile.jpg"
+          />
+          <img src="~assets/projects/awesome-inc/awesome-inc-vc.jpg" alt="visiting card mockup" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-identity-mobile.jpg"
+          />
+          <img
+            src="~assets/projects/awesome-inc/awesome-inc-identity.jpg"
+            alt="business identity mockup"
+          />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-tshirt-mobile.jpg"
+          />
+          <img src="~assets/projects/awesome-inc/awesome-inc-tshirt.jpg" alt="best tshirt design" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="p-image-100-duo">
+      <div class="reveal-holder" data-aos="fade-up" data-aos-once="true">
+        <div class="reveal-block right" data-aos="reveal-right" data-aos-once="true"></div>
+        <picture>
+          <source
+            media="(max-width: 600px)"
+            srcset="~assets/projects/awesome-inc/awesome-inc-tagline-mobile.jpg"
+          />
+          <img src="~assets/projects/awesome-inc/awesome-inc-tagline.jpg" alt="poster" />
+        </picture>
+      </div>
+    </div>
+
+    <div class="keyfocus">
+      <div class="content">
+        <div class="contain">
+          <h2 class="p-para black50">Stuff I did</h2>
+
+          <div class="pm black70">
+            <div class="kf-layout">
+              <span>✔ Research</span>
+              <span>✔ Ideation</span>
+              <span>✔ Creative Direction</span>
+              <span>✔ Design Meeting</span>
+              <span>✔ Iterated many times</span>
+              <span>✔ Lots of IceTea</span>
+              <span>✔ T-shirt</span>
+              <span>✔ Made Mockups</span>
+              <span>✔ Testing with users</span>
+              <span>✔ Creating A design System</span>
+              <span>✔ Branding</span>
+              <span>✔ Identity Design</span>
+              <span>✔ Defending Decisions</span>
+              <span>✔ Pop and un-pop things</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- conclusion -->
+
+    <div class="Takeaway">
+      <div class="contain">
+        <h2 class="p-para black50">Learnings</h2>
+        <div class="pm black70">
+          <p>
+            When It comes to design for a brand that is itself into branding. You got to give your best. No excuses.
+            Thats what happened with Awesome Inc. I loved working with an energetic client and people that were involved.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="p-quote contain red">
+      <blockquote>
+        This project turned out so much fun, i experimented with lots of layout. Now I see it love
+        what it turned out.
+      </blockquote>
+    </div>
+
+    <!-- End Line  -->
+
+    <div class="section-divider">
+      <hr class="section-divider" />
+    </div>
+
+    <!-- Appreciate section -->
+
+    <Appreciate
+      heading="Lets do something awesome together."
+      subHeading="Don't forget to appreciate."
+    />
+
+    <!-- Project Footer Navigation -->
+
+    <div class="project-footer">
+      <div class="projects-footer-container">
+        <allprojectsnav />
+        <div class="p-f-p-container">
+          <nuxt-link to="/projects/dear-zindagi">
+            <pfooterp
+              projectName="Dear Zindagi"
+              projectCategories="Brochure, Identity"
+              thumbnail="/dear-zindagi-thumbnail.webp"
+            />
+          </nuxt-link>
+        </div>
+
+        <div class="p-f-n-container">
+          <nuxt-link to="/projects/tbt-app">
+            <pfootern
+              projectName="TBT App"
+              projectCategories="App, Branding"
+              thumbnail="/tbt-app-thumbnail.jpg"
+            />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Projecthero from '~/components/projects/projecthero'
+import Preface from '~/components/projects/preface'
+import Projectfooter from '~/components/projects/projectfooter'
+import Pcase from '~/components/projects/pcase'
+import Appreciate from '~/components/projects/appreciate'
+import Next from '~/components/projects/next'
+import Previous from '~/components/projects/previous'
+import Pfootern from '~/components/projects/pfootern'
+import Pfooterp from '~/components/projects/pfooterp'
+import Allprojectsnav from '~/components/projects/allprojectsnav'
+export default {
+  layout: 'projects',
+  components: {
+    Projectfooter,
+    Preface,
+    Appreciate,
+    Pcase,
+    Projecthero,
+    Next,
+    Previous,
+    Pfooterp,
+    Pfootern,
+    Allprojectsnav
+  }
+}
+</script>
+
+<style>
+.project-footer {
+  bottom: 0%;
+}
+</style>
